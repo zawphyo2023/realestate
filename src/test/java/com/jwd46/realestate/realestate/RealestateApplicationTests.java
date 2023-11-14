edfed456d91bfe7cae5a2bd4c682a0fe2fe6085d
zawphyo2023 <zawphyo1001@gmail.com>
2023-11-14 13:45:03 +0700

    Initial commit

---
+++ src/test/java/com/jwd46/realestate/realestate/RealestateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwd46.realestate.realestate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealestateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
